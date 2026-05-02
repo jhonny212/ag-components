26e3af3eb19c35fec4779765a388a7c30e68d224
Jhonny garcia <ismaelherz2001@gmail.com>
2026-05-01 19:45:55 -0600

    F

--- core/interfaces/routing/route.interface.ts
+++
@@ -1,7 +0,0 @@
-import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
-import { IBaseMenu } from "../../../../core/interfaces/routing/menu.interface";
-
-export interface IRoute extends IBaseMenu {
-  path: string;
-  icon?: IconDefinition
-}
