5b1eb450c0f0a529eabc9e304a9554e6ec668349
Jhonny garcia <ismaelherz2001@gmail.com>
2026-05-14 19:35:00 -0600

    update shared

--- core/types/color.type.ts
+++ core/types/color.type.ts
@@ -1,3 +1,3 @@
 import { ButtonSeverity } from 'primeng/button';
 
-export type ColorType = ButtonSeverity | 'warning' | 'gray' | 'light' | 'orange' | 'transparent' | 'neutral';
+export type ColorType = ButtonSeverity | 'warning' | 'gray' | 'light' | 'orange' | 'transparent' | 'neutral' | 'custom';
--- library/base/base-components/base-fields.ts
+++ library/base/base-components/base-fields.ts
@@ -16,7 +16,7 @@ export abstract class BaseField {
   inputClass = input<string>('');
   icon = input<IconDefinition | undefined>(undefined);
   isDisabled = input<boolean | undefined>(false);
-  appendTo = input<string | undefined>("self");
+  appendTo = input<string | undefined>("body");
 
   onClick = output<Event>();
 
